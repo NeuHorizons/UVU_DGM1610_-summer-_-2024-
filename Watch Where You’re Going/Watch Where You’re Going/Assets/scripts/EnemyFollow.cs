f78e750047f6088ff7030803ac5d4ec2d4be8adf
Cameron <camhengland@gmail.com>
2024-07-10 15:18:39 -0600

    update to ball game

--- Watch Where You’re Going/Watch Where You’re Going/Assets/scripts/EnemyFollow.cs
+++ Watch Where You’re Going/Watch Where You’re Going/Assets/scripts/EnemyFollow.cs
@@ -20,5 +20,9 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (charactor.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+         if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
